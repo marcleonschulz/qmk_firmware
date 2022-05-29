85d33244871dcd4470360b6140a3fa37f2efebfa
Classix <43107913+marcleonschulz@users.noreply.github.com>
2022-05-29 11:21:58 +0100

    Update rules.mk
    So the keyboard can print "useful" things on the OLED.

--- keyboards/42keebs/mysterium/v15d/rules.mk
+++ keyboards/42keebs/mysterium/v15d/rules.mk
@@ -34,3 +34,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 ENCODER_ENABLE = yes
 OLED_ENABLE = yes
+WPM_ENABLE = yes
+WPM_SAMPLE_SECONDS = 10
